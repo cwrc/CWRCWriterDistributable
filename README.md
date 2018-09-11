c2b4e235cef36ee3a93800341d64c18e81ade818
James Chartrand <jc.chartrand@gmail.com>
2018-09-11 15:53:54 -0400

    chore(all): milestone commit

--- README.md
+++ README.md
@@ -2,4 +2,80 @@
 
 [![experimental](http://badges.github.io/stability-badges/dist/experimental.svg)](http://github.com/badges/stability-badges)
 
-See [CWRC-GitWriter](https://github.com/cwrc/CWRC-GitWriter) for documentation.
+See [CWRC-GitWriter](https://github.com/cwrc/CWRC-GitWriter) for documentation about the GitWriter itself.
+
+This repository exists as a means to build a distributable version of the CWRC-Writer that can be included into the script tag of an html page like so:
+
+```
+<html>
+    <body>
+        <div id="cwrcWriterContainer" style="height: 100%; width: 100%;"></div>
+        <script type="text/javascript" src="js/cwrcwriter.js"></script>
+        <script type="text/javascript" src="js/yourAppThatUsesTheCWRCWRiter.js"></script>
+    </body>
+</html>
+``` 
+
+where cwrcwriter.js contains the distributable CWRCWriter, and yourAppThatUsesTheCWRCWRiter.js (or whatever you want to call your file or files) contains your code.
+
+The cwrc.js file will, when loaded in the scripty tag, expose a constructor in global (window) scope:
+
+window.CWRCWriter
+
+Your code (in yourAppThatUsesTheCWRCWRiter.js) would then create a CWRCWriter like so:
+
+var writer = new CWRCWriter(config)
+
+where the config file is described below in [Config](#config).
+
+The constructor will instantiate a CWRCWriter and add it into the DOM element whose id you list in the config (so you'll also have to include that element in the html page, like we did above):
+
+```config.container = 'cwrcWriterContainer'```
+
+You can then use the 'writerInitialized' event to do whatever needs doing after the CWRCWriter has finished initializing, like say loading a document:
+ 
+writer.event('writerInitialized').subscribe(function() {
+    writer.setDocument(documentAsStringOrParsedXMLDocument)
+});
+
+When you want to get the document out of the CWRC-Writer (say to save it):
+
+writer.getDocument() which will return the string version of the document.
+
+### Config
+
+The CWRC-Writer is configured by passing in a config object to the constructor.   A sample config object is included in the [config.js file](/js/config.js)
+
+Some of the values that can be configured are:
+
+##### Required Options
+
+* `config.container`: String. The ID of the HTML element into which to inject the CWRC-Writer.
+* `config.storageDialogs`: Object. Storage dialogs, see [cwrc-git-dialogs](https://github.com/cwrc/cwrc-git-dialogs) for example and API definition.
+* `config.entityLookupDialogs`: Object. Entity lookup, see [cwrc-public-entity-dialogs](https://github.com/cwrc/CWRC-PublicEntityDialogs) for example and API definition.
+
+##### Other Options
+
+* `config.cwrcRootUrl`: String. An absolute URL that should point to the root of the CWRC-Writer directory. If blank, the browser URL will be used.
+* `config.modules`: Object. The IDs of the [modules](#modules) to load, grouped by their locations relative to the CWRC-Writer.
+  
+  For example:
+  ```
+  config.modules = {
+    west: ['structure','entities'],
+    east: ['selection'],
+    south: ['validation']
+  }
+  ```
+* `config.annotator`: Boolean. If true, the end user may only add annotations to the document.
+* `config.readonly`: Boolean. If true, the end user may not edit nor annotate the document.
+* `config.mode`: String. The mode in which to start the CWRC-Writer.  `xml` or `xmlrdf`.
+* `config.allowOverlap`: Boolean. Should overlapping entities be allowed initially?
+* `config.validationUrl`: String. The URL to use for XML validation. If blank, will default to the validation service provided by CWRC.
+* `config.schemas`: Object. A map of schema objects that can be used in the CWRC-Writer. Each entry should contain the following:
+  * `name`: String. The schema title.
+  * `url`: String. An URL that links to the schema (RELAX NG) file.
+  * `cssUrl`: String. An URL that links to the CSS associated with this schema.
+  * `schemaMappingsId`: String. The directory name in the [schema directory](src/js/schema) from which to load mapping and dialogs files for the schema.
+  * `entityTemplates`: Object. Lists URLs for use by citation and note entity dialogs.
+* `config.buttons1`, `config.buttons2`, `config.buttons3`: String. A comma separated list of plugins to set in the CWRC-Writer toolbars. Possible values: `addperson, addplace, adddate, addorg, addcitation, addnote, addtitle, addcorrection, addkeyword, addlink, editTag, removeTag, addtriple, viewsource, editsource, validate, savebutton, loadbutton`.
--- src/js/config.js
+++ src/js/config.js
@@ -1,56 +1,103 @@
+// the dialogs with which the end user interacts, to load from and save to Github repositories
+const CWRCWriterStorageDialogs = require('cwrc-git-dialogs')['default'];
+
+// the dialogs with which the end user interacts, to lookup entities in public sources:
+const CWRCWriterDialogs = require('cwrc-public-entity-dialogs');
+
+// configure the entity lookups a bit:
+CWRCWriterDialogs.showNoLinkButton(true);
+CWRCWriterDialogs.showCreateNewButton(false);
+CWRCWriterDialogs.showEditButton(false);
+
+// the specific source lookups that we pass into the entity lookup dialogs:
+const viaf = require('viaf-entity-lookup')
+const dbpedia = require('dbpedia-entity-lookup');
+const wikidata = require('wikidata-entity-lookup');
+const getty = require('getty-entity-lookup');
+const geonames = require('geonames-entity-lookup');
+
+// and here we pass those source lookups into the dialogs, setting them in one Map for each entity type:
+CWRCWriterDialogs.registerEntitySources({
+	person: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('getty', getty).set('dbpedia', dbpedia),
+	place: (new Map()).set('geonames', geonames).set('viaf', viaf).set('dbpedia', dbpedia).set('wikidata', wikidata),
+	organization: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia),
+	title: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia)
+})
+
+// the id of the html element into which to insert the CWRC-Writer:
+const container = 'cwrcWriterContainer';
+
+// which CWRCWriter modules to include in the writer UI and how to arrange them in the UI
+const modules = {
+	west: [ 'structure', 'entities', 'relations' ],
+	south: [ 'selection', 'validation' ],
+	east: [ 'imageViewer' ]
+};
+
+// the url for the CWRC XML validator, to which the CWRC-Writer makes ajax calls to validate the document against the schema
+const validationURL = "https://validator.services.cwrc.ca/validator/validate.html"
+
+// the schemas supported, along with their relax ng file, css file for styling, and templates for editing notes and citations
+const schemas = {
+	"tei": {
+		"name": "CWRC Basic TEI Schema",
+		"url": "https://cwrc.ca/schemas/cwrc_tei_lite.rng",
+		"cssUrl": "https://cwrc.ca/templates/css/tei.css",
+		"schemaMappingsId": "tei",
+		"entityTemplates": {
+			"note": "https://dev-cwrc-writer.cwrc.ca/schema/tei/xml/note.xml",
+			"citation": "https://dev-cwrc-writer.cwrc.ca/schema/tei/xml/citation.xml"
+		}
+	},
+	"events": {
+		"name": "Events Schema",
+		"url": "https://cwrc.ca/schemas/orlando_event_v2.rng",
+		"cssUrl": "https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css",
+		"schemaMappingsId": "orlando",
+		"entityTemplates": {
+			"note": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/note_events.xml",
+			"citation": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/citation_events.xml"
+		}
+	},
+	"biography": {
+		"name": "Biography Schema",
+		"url": "https://cwrc.ca/schemas/orlando_biography_v2.rng",
+		"cssUrl": "https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css",
+		"schemaMappingsId": "orlando",
+		"entityTemplates": {
+			"note": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/note_biography.xml",
+			"citation": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/citation_biography.xml"
+		}
+	},
+	"writing": {
+		"name": "Writing Schema",
+		"url": "https://cwrc.ca/schemas/orlando_writing_v2.rng",
+		"cssUrl": "https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css",
+		"schemaMappingsId": "orlando",
+		"entityTemplates": {
+			"note": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/note_writing.xml",
+			"citation": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/citation_writing.xml"
+		}
+	},
+	"cwrcEntry": {
+		"name": "CWRC Entry Schema",
+		"url": "https://cwrc.ca/schemas/cwrc_entry.rng",
+		"cssUrl": "https://cwrc.ca/templates/css/cwrc.css",
+		"schemaMappingsId": "cwrcEntry",
+		"entityTemplates": {
+			"note": "https://dev-cwrc-writer.cwrc.ca/schema/cwrcEntry/xml/note.xml",
+			"citation": "https://dev-cwrc-writer.cwrc.ca/schema/cwrcEntry/xml/citation.xml"
+		}
+	}
+}
+
+// finally, set all this in an object that we return.
 module.exports = {
+    "container": container,
+    "modules": modules,
+    "entityLookupDialogs": CWRCWriterDialogs,
+    "storageDialogs": CWRCWriterStorageDialogs,
     "cwrcRootUrl": "",
-    "validationUrl": "https://validator.services.cwrc.ca/validator/validate.html",
-    "schemas": {
-        "tei": {
-            "name": "CWRC Basic TEI Schema",
-            "url": "https://cwrc.ca/schemas/cwrc_tei_lite.rng",
-            "cssUrl": "https://cwrc.ca/templates/css/tei.css",
-            "schemaMappingsId": "tei",
-            "entityTemplates": {
-                "note": "https://dev-cwrc-writer.cwrc.ca/schema/tei/xml/note.xml",
-                "citation": "https://dev-cwrc-writer.cwrc.ca/schema/tei/xml/citation.xml"
-            }
-        },
-        "events": {
-            "name": "Events Schema",
-            "url": "https://cwrc.ca/schemas/orlando_event_v2.rng",
-            "cssUrl": "https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css",
-            "schemaMappingsId": "orlando",
-            "entityTemplates": {
-                "note": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/note_events.xml",
-                "citation": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/citation_events.xml"
-            }
-        },
-        "biography": {
-            "name": "Biography Schema",
-            "url": "https://cwrc.ca/schemas/orlando_biography_v2.rng",
-            "cssUrl": "https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css",
-            "schemaMappingsId": "orlando",
-            "entityTemplates": {
-                "note": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/note_biography.xml",
-                "citation": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/citation_biography.xml"
-            }
-        },
-        "writing": {
-            "name": "Writing Schema",
-            "url": "https://cwrc.ca/schemas/orlando_writing_v2.rng",
-            "cssUrl": "https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css",
-            "schemaMappingsId": "orlando",
-            "entityTemplates": {
-                "note": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/note_writing.xml",
-                "citation": "https://dev-cwrc-writer.cwrc.ca/schema/orlando/xml/citation_writing.xml"
-            }
-        },
-        "cwrcEntry": {
-            "name": "CWRC Entry Schema",
-            "url": "https://cwrc.ca/schemas/cwrc_entry.rng",
-            "cssUrl": "https://cwrc.ca/templates/css/cwrc.css",
-            "schemaMappingsId": "cwrcEntry",
-            "entityTemplates": {
-                "note": "https://dev-cwrc-writer.cwrc.ca/schema/cwrcEntry/xml/note.xml",
-                "citation": "https://dev-cwrc-writer.cwrc.ca/schema/cwrcEntry/xml/citation.xml"
-            }
-        }
-    }
+    "validationUrl": validationURL,
+    "schemas": schemas
 };
